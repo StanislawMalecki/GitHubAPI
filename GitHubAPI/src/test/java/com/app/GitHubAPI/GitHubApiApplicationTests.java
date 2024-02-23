a3e986fb4b9249924b75e717dade96940d3f628f
StanislawMalecki <stas.malecki323@gmail.com>
2024-02-23 17:50:10 +0100

    Header support added, exception handler and README

--- GitHubAPI/src/test/java/com/app/GitHubAPI/GitHubApiApplicationTests.java
+++ GitHubAPI/src/test/java/com/app/GitHubAPI/GitHubApiApplicationTests.java
@@ -1,13 +1,21 @@
 package com.app.GitHubAPI;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GitHubApiApplicationTests {
-
+	@Autowired
+	private GitHubController controller;
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
-
 }
+
